f253f83086b90d5321c16f09cbbf7a73d849a039
AyoyH <ayah28603@gmail.com>
2025-03-01 02:00:39 +0200

    vercel.json added

--- src/Database/DBConnection/dbConnection.js
+++ src/Database/DBConnection/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnetion = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URI_ONLINE);
     console.log("Database connected successfully");
   } catch (error) {
     console.log(`error in database ${error}`);
